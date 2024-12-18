06b2c92a675fd6d4289057654256fea7c4db64cc
Jonathan <jonathanfmach@gmail.com>
2024-12-18 12:25:59 +0000

    Initial commit

---
+++ vue-electron-capacitor-experiment/android/app/src/main/java/vue/electron/capacitor/experiment/MainActivity.java
@@ -0,0 +1,5 @@
+package vue.electron.capacitor.experiment;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ vue-electron-capacitor-experiment/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vue.electron.capacitor.experiment',
+  appName: 'vue-electron-capacitor-experiment',
+  webDir: 'dist'
+};
+
+export default config;
